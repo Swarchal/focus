fc9763861597cd9ec16b4ae4d69d0e73716ccf9b
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-14 19:36:30 +0000

    add test and remove old data

--- R/df.R
+++
@@ -1,8 +0,0 @@
-#' Sample dataset from CellProfiler
-#'
-#' Image dataset, containing ImageQuality columns.
-#'
-#' @format A dataframe with rows and columns
-#' @source Screen performed by Scott Warchal, output picked at random.
-
-"df"
